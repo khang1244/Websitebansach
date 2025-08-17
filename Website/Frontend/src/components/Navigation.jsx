15009ad158239a63868ccb763e0cfb46380cfb24
khang1244 <96627298+khang1244@users.noreply.github.com>
2025-08-17 21:22:46 +0700

    Tạo Thanh Điều Hướng

---
+++ Website/Frontend/src/components/Navigation.jsx
@@ -0,0 +1,27 @@
+import logo from "../assets/Red Panda.png";
+import { FaShoppingCart } from "react-icons/fa";
+function Navigation() {
+  return (
+    <nav className="px-6 py-6 flex justify-between items-center bg-[#00809D] text-white">
+      <div className="flex items-center space-x-12">
+        <div>
+          <img src={logo} alt="logo" className="h-8" />
+        </div>
+        <ul className="flex space-x-4 justify-end items-center font-semibold">
+          <li>Trang Chủ</li>
+          <li>Giới Thiệu</li>
+          <li>Dịch Vụ</li>
+          <li>Liên Hệ</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4">
+        <FaShoppingCart className="text-2xl" />
+        <div className="space-x-0">
+          <button className="bg-white text-black px-3 py-2 rounded-full font-semibold" >Đăng nhập</button>
+          <button className="hidden">Đăng xuất</button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default Navigation;
